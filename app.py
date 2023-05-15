0218971db06d639b34486f0d47362c139739b5f0
Nabegha Mazahir <mnabegha@MacBook.local>
2023-05-15 21:06:00 +0930

    Variable updated

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import os
 from flask import Flask, redirect
 from dotenv import load_dotenv
-
-
 from routes.blog_routes import blog_routes
 from routes.users_routes import users_routes
 from routes.sessions_routes import sessions_routes
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('API_KEYS')
-
 
 app = Flask(__name__)
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "pretend key for testing only")
 app.config['SECRET_KEY'] = SECRET_KEY
 
 app.register_blueprint(blog_routes, url_prefix='/blog')
